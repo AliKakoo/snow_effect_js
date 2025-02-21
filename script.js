0915263fcc20db67bc1c064fcd7958a4ef8a8408
AliKakoo <alikakuo@yahoo.com>
2025-02-21 11:53:39 +0330

    add feature and timeout

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("click", (event) => {
+
+  let snowSpan = document.createElement("span");
+  snowSpan.classList.add("snowflake");
+
+
+  snowSpan.style.left = event.clientX + "px";
+  snowSpan.style.top = event.clientY + "px";
+
+  let size = Math.floor(Math.random()*140)
+
+  snowSpan.style.width = size + 'px'
+  snowSpan.style.height = size + 'px'
+
+  document.body.appendChild(snowSpan);
+
+  setTimeout(() =>{
+    snowSpan.remove();
+  },1000)
+});
